24413c52ed08338a3d61a2c13c8d1bc8f9d3abab
LaMeraChuleta <geoffreyytorres25@gmail.com>
2020-07-31 13:09:57 -0500

    CambiosEnValidacionesErrores

--- client/src/main.rs
+++ client/src/main.rs
@@ -8,18 +8,27 @@ const LOCAL: &str = "10.0.0.7:5000";
 const MSG_SIZE: usize = 32;
 
 fn main() {
-    let mut client = TcpStream::connect(LOCAL).expect("Stream failed to connect");
-    client.set_nonblocking(true).expect("failed to initiate non-blocking");
 
-    let (tx, rx) = mpsc::channel::<String>();
+    let mut client = TcpStream::connect(LOCAL).expect("TCPstream no creado");
+    client
+        .set_nonblocking(true)
+        .expect("No se ejecuto set_nonblocking");
+
+    let (sender, reciver) = mpsc::channel::<String>();
 
     thread::spawn(move || loop {
         let mut buff = vec![0; MSG_SIZE];
+
+        //Estara leyendo reciviendo los mensajes del service para confimar que el mensaje llego
         match client.read_exact(&mut buff) {
             Ok(_) => {
-                let msg = buff.into_iter().take_while(|&x| x != 0 ).collect::<Vec<_>>();
-                println!("message recv {:?}", msg.into_iter().map(|i| i.to_string()).collect::<String>());
-            },
+                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                
+                println!(
+                    "Mensaje Recibido en el Servidor {:?}",
+                    msg
+                );
+            }
             Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
             Err(_) => {
                 println!("connection with server was severed");
@@ -27,27 +36,31 @@ fn main() {
             }
         }
 
-        match rx.try_recv() {
+        //Rebisara los mensajes que estamos enviando
+        match reciver.try_recv() {
             Ok(msg) => {
                 let mut buff = msg.clone().into_bytes();
                 buff.resize(MSG_SIZE, 0);
-                client.write_all(&buff).expect("writing to socket failed");
-                println!("message sent {:?}", msg);
-            }, 
+                client.write_all(&buff).expect("escritura fallida en el socket");
+                println!("Mensaje Enviado {:?}", msg);
+            }
             Err(TryRecvError::Empty) => (),
-            Err(TryRecvError::Disconnected) => break
+            Err(TryRecvError::Disconnected) => break,
         }
 
         thread::sleep(Duration::from_millis(100));
     });
 
-    println!("Write a Message:");
+    println!("Escriba el mensaje:");
     loop {
         let mut buff = String::new();
-        io::stdin().read_line(&mut buff).expect("reading from stdin failed");
+        io::stdin()
+            .read_line(&mut buff)
+            .expect("fallo al leer el mensaje");
         let msg = buff.trim().to_string();
-        if msg == ":quit" || tx.send(msg).is_err() {break}
+        if msg == ":quit" || sender.send(msg).is_err() {
+            break;
+        }
     }
     println!("bye bye!");
-
 }
--- service/src/main.rs
+++ service/src/main.rs
@@ -10,35 +10,34 @@ fn sleep() {
     thread::sleep(::std::time::Duration::from_millis(100));
 }
 fn main() {
-    
-
-    let server = TcpListener::bind(LOCAL).expect("no se pudo conectar a la Ip");
-
-    server.set_nonblocking(true).expect("no se pudo realizar el set_non");
+    let server = TcpListener::bind(LOCAL).expect("No se conecto a la Ip");
+    server
+        .set_nonblocking(true)
+        .expect("No se ejecuto set_nonblocking");
 
     let mut clients = vec![];
     let (sender, receiver) = mpsc::channel::<String>();
     loop {
         if let Ok((mut socket, addr)) = server.accept() {
-            println!("Cliente: {} se conecto", addr);
+            println!("Cliente: {} Conectado", addr);
 
             let sender = sender.clone();
-            clients.push(socket.try_clone().expect("fallo clonando el cliente"));
+            clients.push(socket.try_clone().expect("Fallo clonando el cliente"));
 
             thread::spawn(move || loop {
-
                 let mut buff = vec![0; MSG_SIZE];
 
                 match socket.read_exact(&mut buff) {
                     Ok(_) => {
                         let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
-                        let msg = String::from_utf8(msg).expect("Caracter UTF8 invalido");
+                        let msg = String::from_utf8(msg).expect("Caracter UTF8 invalido");                        
                         println!("{}: {:?}", addr, msg);
-                        sender.send(msg).expect("fallo al enviar");
-                    }, 
+                        //Envia el mensaje
+                        sender.send(msg).expect("Fallo al enviar");
+                    }
                     Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
                     Err(_) => {
-                        println!("El cliente: {} se desconecto", addr);
+                        println!("Cliente: {} Desconectado", addr);
                         break;
                     }
                 }
@@ -48,17 +47,16 @@ fn main() {
         }
 
         if let Ok(msg) = receiver.try_recv() {
-            clients = clients.into_iter().filter_map(|mut client| {
-
-                println!("{:?}", msg);
-                let mut buff = msg.clone().into_bytes();
-                buff.resize(MSG_SIZE, 0);
-
-                client.write_all(&buff).map(|_| client).ok()
-            }).collect::<Vec<_>>();
-            
+            println!("{}", msg);
+            clients = clients
+                .into_iter()
+                .filter_map(|mut client| {
+                    let mut buff = msg.clone().into_bytes();
+                    buff.resize(MSG_SIZE, 0);
+                    client.write_all(&buff).map(|_| client).ok()
+                })
+                .collect::<Vec<_>>();
         }
-
         sleep();
     }
 }
