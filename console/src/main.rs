4b4231c0d2fd5791b9f560a0776a602078ca3198
unknown <geoffreyytorres25@gmail.com>
2021-06-11 12:12:01 -0500

    Tabla y Grafica

--- console/src/main.rs
+++ console/src/main.rs
@@ -3,7 +3,8 @@ use tui::Terminal;
 use tui::backend::CrosstermBackend;
 use tui::text::{ Span, Spans, Text };
 use tui::style::{ Style, Color, Modifier };
-use tui::widgets::{ Block, Borders, Paragraph, BorderType, List, ListItem, Wrap };
+use tui::symbols;
+use tui::widgets::{ Block, Borders, Paragraph, BorderType, List, ListItem, Wrap, Axis, Dataset, Chart, GraphType, Row, Cell, Table };
 use tui::layout::{ Layout, Constraint, Direction, Rect, Alignment };
 fn main() -> Result<(), io::Error>{
     let stdout = io::stdout();
@@ -11,55 +12,137 @@ fn main() -> Result<(), io::Error>{
     let mut terminal = Terminal::new(backend)?;
     terminal.clear()?;
     loop {                
-        terminal.draw(|f| {                         
+        terminal.draw(|f| {           
+            //Pricnipal
             let chunks = Layout::default()
-                .margin(1)
-                .direction(Direction::Horizontal)
+                .margin(1)                   
+                .direction(Direction::Vertical)                
                 .constraints([
                         Constraint::Percentage(20),
                         Constraint::Percentage(80)
                     ].as_ref())
                 .split(f.size());
 
-            let copyright = Paragraph::new("pet-CLI 2020 - all rights reserved")
+            let copyright = Paragraph::new("BATTLE SHIP RUSTEADO")
                 .style(Style::default().fg(Color::LightCyan))
                 .alignment(Alignment::Center)
                 .block(
-                    Block::default()
-                    .title("Block")
-                    .borders(Borders::LEFT | Borders::RIGHT)
+                    Block::default()                    
+                    .borders(Borders::ALL)
+                    .title("Block")                    
                     .border_style(Style::default().fg(Color::White))
                     .border_type(BorderType::Rounded)
                     .style(Style::default().bg(Color::Black))                       
-                );  
-            // let text = vec![
-            //         Spans::from(vec![
-            //             Span::raw("Battle"),
-            //             Span::styled("Ship",Style::default().add_modifier(Modifier::ITALIC)),
-            //             Span::raw("."),
-            //         ]),
-            //         Spans::from(Span::styled("Rusteado", Style::default().fg(Color::Red))),
-            //     ];
-            // let titulo = Paragraph::new(text)
-            //         .block(Block::default().borders(Borders::ALL))
-            //         .style(Style::default().fg(Color::White).bg(Color::Black))
-            //         .alignment(Alignment::Center)
-            //         .wrap(Wrap { trim: true });  
+                );            
             f.render_widget(copyright, chunks[0]);                                                                   
                               
             let chunks2 = Layout::default()
-                .constraints([Constraint::Percentage(500), Constraint::Percentage(50)].as_ref())
+                .constraints([Constraint::Percentage(70), Constraint::Percentage(30)].as_ref())
                 .direction(Direction::Horizontal)
-                .split(chunks[1] ); 
-            let items = [ListItem::new("Item 1"), ListItem::new("Item 2"), ListItem::new("Item 3")];
-                
-            let tasks = List::new(items)
-                .block(Block::default().borders(Borders::ALL).title("List"))
-                .highlight_style(Style::default().add_modifier(Modifier::BOLD))
-                .highlight_symbol("> ");
+                .split(chunks[1]);
+            {
+                let chunks2 = Layout::default()
+                    .constraints([Constraint::Percentage(120), Constraint::Percentage(50)].as_ref())
+                    .direction(Direction::Horizontal)
+                    .split(chunks2[0]);
+                {
+                    let chunks2 = Layout::default()
+                        .constraints([Constraint::Percentage(120), Constraint::Percentage(50)].as_ref())
+                        .direction(Direction::Horizontal)
+                        .split(chunks2[0]);  
+                    
+                    let datasets = vec![
+                        Dataset::default()
+                            .name("data1")
+                            .marker(symbols::Marker::Block)
+                            .graph_type(GraphType::Scatter)
+                            .style(Style::default().fg(Color::Cyan))
+                            .data(&[(0.0, 5.0),(0.0, 6.0)]),
+                        Dataset::default()
+                            .name("data2")
+                            .marker(symbols::Marker::Block)
+                            .graph_type(GraphType::Scatter)
+                            .style(Style::default().fg(Color::Magenta))
+                            .data(&[(2.0, 5.0),(2.0, 7.0)]),
+                    ];
 
+                    let tablero = Chart::new(datasets)       
+                    .block(      Block::default()                    
+                        .borders(Borders::ALL)
+                        .title("Tablero")                    
+                        .border_style(Style::default().fg(Color::White))
+                        .border_type(BorderType::Rounded)
+                        .style(Style::default().bg(Color::Black))
+                    )
+                    .x_axis(Axis::default()
+                        // .title(Span::styled("X Axis", Style::default().fg(Color::Red)))
+                        .style(Style::default().fg(Color::White))
+                        .bounds([0.0, 10.0])
+                        .labels(["0.0", "1.0", "2.0", "3.0", "4.0", "5.0", "6.0", "7.0", "8.0", "9.0", "10.0"].iter().cloned().map(Span::from).collect()))
+                    .y_axis(Axis::default()
+                        // .title(Span::styled("Y Axis", Style::default().fg(Color::Red)))
+                        .style(Style::default().fg(Color::White))
+                        .bounds([0.0, 10.0])
+                        .labels(["0.0", "1.0", "2.0", "3.0", "4.0", "5.0", "6.0", "7.0", "8.0", "9.0", "10.0"].iter().cloned().map(Span::from).collect()));
+
+                    f.render_widget(tablero, chunks2[0]);             
+                }                   
+            }  
+            let tabla = Table::new(vec![
+                // Row can be created from simple strings.
+                Row::new(vec!["", "1", "2", "3","4","5","6","7","8","9"]).style(Style::default().fg(Color::Blue)).height(1),
+                // You can style the entire row.
+                
+                // If you need more control over the styling you may need to create Cells directly
+                Row::new(vec![
+                    Cell::from("Row31"),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),                    
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                ]),
+                // If a Row need to display some content over multiple lines, you just have to change
+                // its height.
+                Row::new(vec![
+                    Cell::from("Row\n41"),
+                    Cell::from("Row\n42"),
+                    Cell::from("Row\n43"),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+                ]).height(2),
+            ])
+            // You can set the style of the entire Table.
+            .style(Style::default().fg(Color::White))
+            // It has an optional header, which is simply a Row always visible at the top.
+            .header(
+                Row::new(vec!["", "1", "2", "3","4","5","6","7","8","9"])
+                    .style(Style::default().fg(Color::Yellow))
+                    // If you want some space between the header and the rest of the rows, you can always
+                    // specify some margin at the bottom.
+                    .bottom_margin(1)
+            )
+            // As any other widget, a Table can be wrapped in a Block.
+            .block(Block::default().title("Table"))
+            // Columns widths are constrained in the same way as Layout...
+            .widths(&[Constraint::Length(5), Constraint::Length(5), Constraint::Length(5),Constraint::Length(5), Constraint::Length(5), Constraint::Length(5),Constraint::Length(5), Constraint::Length(5), Constraint::Length(5),Constraint::Length(5)])
+            // ...and they can be separated by a fixed spacing.
+            .column_spacing(1)
+            // If you wish to highlight a row in any specific way when it is selected...
+            .highlight_style(Style::default().add_modifier(Modifier::BOLD))
+            // ...and potentially show a symbol in front of the selection.
+            .highlight_symbol(">>");
             
-            f.render_widget(tasks, chunks[1]);                                         
+            let block = Block::default().borders(Borders::ALL).title("Graphs");     
+            f.render_widget(tabla, chunks2[1]);
         })?;
         terminal.autoresize()?;
     }    
